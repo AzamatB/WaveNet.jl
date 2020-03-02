2e43b742d9fcbce2c60cbd0119a89d4b2d55ea01
Azamat Berdyshev <aberdysh@gmail.com>
2020-03-03 02:16:07 +0600

    complete revamp, notably remove mutation

--- WaveNet.jl
+++ WaveNet.jl
@@ -7,16 +7,14 @@ using NamedTupleTools
    tanh.(x[:, 1:split, :]) .* σ.(x[:, (split+1):end, :])
 end
 
-struct ResBlock{T<:DenseArray{<:Real,3}, D, R, S, C}
-   skip     :: Ref{T}
+struct ResBlock{D, R, S, C}
    dilconv  :: D
    resconv  :: R
    skipconv :: S
    condconv :: C
 end
 
-struct LastBlock{T<:DenseArray{<:Real,3}, D, S, C}
-   skip     :: Ref{T}
+struct LastBlock{D, S, C}
    dilconv  :: D
    skipconv :: S
    condconv :: C
@@ -26,60 +24,53 @@ end
 @functor LastBlock
 
 function ResBlock(dilation::Integer, nch::NamedTuple{(:res, :skip, :cond),<:NTuple{3,Integer}})
-   skip = Ref(gpu(zeros(Float32, 0, 0, 0)))
    dilconv  = Conv((2,), nch.res => 2nch.res; dilation = dilation, pad = (dilation, 0)) |> gpu
    resconv  = Conv((1,), nch.res => nch.res)  |> gpu
    skipconv = Conv((1,), nch.res => nch.skip) |> gpu
    condconv = Conv((1,), nch.cond => 2nch.res, tanh) |> gpu
-   ResBlock(skip, dilconv, resconv, skipconv, condconv)
+   ResBlock(dilconv, resconv, skipconv, condconv)
 end
 
 function LastBlock(dilation::Integer, nch::NamedTuple{(:res, :skip, :cond),<:NTuple{3,Integer}})
-   skip = Ref(gpu(zeros(Float32, 0, 0, 0)))
    dilconv  = Conv((2,), nch.res => 2nch.res; dilation = dilation, pad = (dilation, 0)) |> gpu
    skipconv = Conv((1,), nch.res => nch.skip) |> gpu
    condconv = Conv((1,), nch.cond => 2nch.res, tanh) |> gpu
-   LastBlock(skip, dilconv, skipconv, condconv)
+   LastBlock(dilconv, skipconv, condconv)
 end
 
-"""                                  residual
+"""                             residual connection
         ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
         ┃                              ┏━━ tanh ━━┓                       ┃
 input ━━┻━━> 2×1 dilatedconv ━━━ + ━━━ ❖          × ━━┳━━ 1×1 resconv ━━━ + ━━━> output
                                  ┃     ┗━━━ σ ━━━━┛   ┃
 conditional input ━━━> 1×1 condconv             1×1 skipconv
                                                       ┃
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━> + ━━━━━━━━> skip
+Σskip ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━> + ━━━━━━━━> Σskip
 """
-function (m::ResBlock)(x::DenseArray{<:Real,3})
+function (m::ResBlock)((Σskip, x)::NTuple{2,DenseArray{<:Real,3}})
    y = gate(m.dilconv(x))
-   m.skip[] = m.skipconv(y)
-   return x + m.resconv(y)
+   return (Σskip + m.skipconv(y), x + m.resconv(y))
 end
 
-function (m::ResBlock)((x, h)::NTuple{2,DenseArray{<:Real,3}})
+function (m::ResBlock)((Σskip, x, h)::NTuple{3,DenseArray{<:Real,3}})
    y = gate(m.dilconv(x) + m.condconv(h))
-   m.skip[] = m.skipconv(y)
-   return (x + m.resconv(y), h)
+   return (Σskip + m.skipconv(y), x + m.resconv(y), h)
 end
 
-function (m::LastBlock)(x::DenseArray{<:Real,3})
+function (m::LastBlock)((Σskip, x)::NTuple{2,DenseArray{<:Real,3}})
    y = gate(m.dilconv(x))
-   m.skip[] = m.skipconv(y)
+   return Σskip + m.skipconv(y)
 end
 
-function (m::LastBlock)((x, h)::NTuple{2,DenseArray{<:Real,3}})
+function (m::LastBlock)((Σskip, x, h)::NTuple{3,DenseArray{<:Real,3}})
    y = gate(m.dilconv(x) + m.condconv(h))
-   m.skip[] = m.skipconv(y)
+   return Σskip + m.skipconv(y)
 end
-
-skip(m::ResBlock)  = m.skip[]
-skip(m::LastBlock) = m.skip[]
 
 struct WaveNet{I, R, O}
-   inputnet  :: I
-   resnet!   :: R
-   outputnet :: O
+   inblock   :: I
+   resblocks :: R
+   outblock  :: O
 end
 
 @functor WaveNet
@@ -88,22 +79,33 @@ function WaveNet(nlayers::Integer,
       nch::NamedTuple{(:res, :skip, :cond, :out),<:NTuple{4,Integer}} = (res=64, skip=256, cond=0, out=256); depth::Integer = 10
    )
    nch′ = delete(nch, :out)
-   inputnet = Conv((2,), 1 => nch.res, tanh; pad=(1,0)) |> gpu
+   inblock = Conv((2,), 1 => nch.res, tanh; pad=(1,0)) |> gpu
 
-   resnet! = Chain(
+   resblocks = Chain(
       (ResBlock(2^(i % depth), nch′) for i ∈ 0:(nlayers-2))...,
       LastBlock(2^((nlayers-1) % depth), nch′)
    ) |> gpu
 
-   outputnet = Chain(
+   outblock = Chain(
       Conv((1,), nch.skip => nch.out, leakyrelu),
       Conv((1,), nch.out => nch.out),
       softmax
    ) |> gpu
-   WaveNet(inputnet, resnet!, outputnet)
+   WaveNet(inblock, resblocks, outblock)
+end
+
+function (m::WaveNet)(x::DenseArray{<:Real,3})
+   y = m.inblock(x)
+   len, _, batch = size(y)
+   skip₀ = gpu(zeros(Float32, len, nch.skip, batch))
+   Σskip = m.resblocks((skip₀, y))
+   return m.outblock(leakyrelu.(Σskip))
 end
 
 function (m::WaveNet)(x::DenseArray{<:Real,3}, h::DenseArray{<:Real,3})
-   m.resnet!((m.inputnet(x), h))
-   m.outputnet(leakyrelu.(sum(skip, m.resnet!)))
+   y = m.inblock(x)
+   len, _, batch = size(y)
+   skip₀ = zeros(Float32, len, nch.skip, batch)
+   Σskip = m.resblocks((skip₀, y, h))
+   return m.outblock(leakyrelu.(Σskip))
 end
--- test.jl
+++ test.jl
@@ -2,16 +2,34 @@ nch = (res=3, skip=5, cond=7)
 dilation = 4
 m = ResBlock(dilation, nch)
 θ = params(m)
-X = rand(Float32, 17, 3, 11)
-H = rand(Float32, 17, 7, 11)
-XH = (X, H)
+len, batch = 17, 11
+x = rand(Float32, len, nch.res, batch)
+h = rand(Float32, len, nch.cond, batch)
+skip₀ = zeros(Float32, len, nch.skip, batch)
 
-m(X)
-m(XH)
+m((skip₀, x))
+m((skip₀, x, h))
 
 gradient(θ) do
-    sum(m(X) .* 1)
+   sum(m((skip₀, x))[1] .* 1) + sum(m((skip₀, x))[2] .* 1)
 end
 gradient(θ) do
-    sum(m(XH)[1] .* 1) + sum(m(XH)[2] .* 1)
+   sum(m((skip₀, x, h))[1] .* 1) + sum(m((skip₀, x, h))[2] .* 1) + sum(m((skip₀, x, h))[3] .* 1)
+end
+
+
+nch = (res=3, skip=5, cond=7, out=11)
+m = WaveNet(13, nch)
+θ = params(m)
+
+x = rand(Float32, len, 1, batch)
+h = rand(Float32, len, nch.cond, batch)
+
+m(x)
+m(x, h)
+gradient(θ) do
+   sum(m(x) .* 1)
+end
+gradient(θ) do
+   sum(m(x, h) .* 1)
 end
